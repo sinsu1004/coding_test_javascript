12dc89542e5621966b2e1524fadee3b1dc8874a5
sinsu <sinsu19012@gmail.com>
2022-10-13 20:47:14 +0900

    20221013

---
+++ back_level3/b_1074.py
@@ -0,0 +1,26 @@
+from sys import stdin
+
+
+input= stdin.readline
+
+n,r,c=map(int,input().rstrip().split())
+
+vist=0
+while n != 0 :
+    n-=1
+    size=2**n
+    
+    if r<size and c < size:
+        vist+=0
+    
+    elif r<size and c>=size:
+        vist+=size*size
+        c -=size
+    elif r>=size and c < size:
+        vist+=size*size*2
+        r -=size
+    else:
+        vist+=size*size*3
+        r-=size
+        c-=size
+print(vist)
