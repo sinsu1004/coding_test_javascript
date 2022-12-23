8bea8502b84bed902dcb973daf34cde583d0ffb2
sinsu <sinsu19012@gmail.com>
2022-12-23 11:41:54 +0900

    20221223

--- back_level1/b_2475.py
+++ back_level1/b_2475.py
@@ -1,5 +1,8 @@
-data =list(map(int,input().split()))
-sum=0
+data = list(map(int, input().split()))
+sum = 0
+
 for i in range(len(data)):
-    sum+=data[i]**2
-print(sum%10)
+
+    sum += data[i]**2
+
+print(sum % 10)
