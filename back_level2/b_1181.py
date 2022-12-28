c5beade0f47cb79a735d55a24b119e316b3f822a
sinsu <sinsu19012@gmail.com>
2022-12-28 19:29:33 +0900

    20221228

---
+++ back_level2/b_1181.py
@@ -0,0 +1,13 @@
+n = int(input())
+lst = list()
+for _ in range(n):
+    lst.append(input())
+
+set_lst = set(lst)
+lst = list(set_lst)
+
+lst.sort()
+lst.sort(key=len)
+
+for i in lst:
+    print(i)
